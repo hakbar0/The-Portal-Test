b5ce2d65d7e7f3b22750da09e2d20efeb036b5e4
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-03 23:07:20 +0100

    Finished database testing.

--- spec/shelfPicker.spec.js
+++ spec/shelfPicker.spec.js
@@ -9,8 +9,43 @@ describe('Database Test', () => {
     })
   })
   it('Able to retrieve all orders in an object format test 2.', () => {
-    db.ref("/HistorySingleShelf").once("value", order => {
-      expect(typeof order.val()).to.equal('object');
+    db.ref("/HistorySingleShelf").once("value", orders => {
+      expect(typeof orders.val()).to.equal('object');
+    })
+  })
+  it('Expect the orders to have the keys, date, pick_location, product_code, quantity.', () => {
+    return db.ref("/HistoryDoubleShelf").once("value", orders => {
+      orderKeys = Object.keys((Object.values(orders.val()))[0]);
+    }).then(somthing => {
+      expect(orderKeys).to.eql(['Date', 'pick_Location', 'product_code', 'quantity']);
+    })
+  })
+  it('Updates total number of orders in the database by 1.', () => {
+    return db.ref("/HistoryDoubleShelf").once("value", orders => {
+    }).then(orders => {
+      orderLength = Object.entries(orders.val()).length;
+    }).then(placeHolder => {
+      return db.ref('/HistoryDoubleShelf').push({
+        pick_Location: 'AB 4',
+        product_code: '12345',
+        quantity: '10',
+        Date: new Date(Date.now()).toISOString()
+      }).then(placeHolder => {
+        return db.ref("/HistoryDoubleShelf").once("value", orders => {
+        }).then(newOrder => {
+          newOrderLength = Object.entries(newOrder.val()).length;
+        }).then(placeHolder => {
+          expect(orderLength + 1).to.equal(newOrderLength)
+        })
+      })
     })
   })
 });
+
+
+
+describe('Inquirer prompt test', () => {
+  it('test', () =>{
+    expect(5).to.equal(5);
+  })
+})
