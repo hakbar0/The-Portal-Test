a9b6835ca3216f87de9169c1d101bccc3a91d1e3
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-03 16:11:09 +0100

    Able to write to csv file. Now need to write the right stuff.

--- components/orderPickLocation.js
+++ components/orderPickLocation.js
@@ -1,14 +1,21 @@
 const order = [];
+var fs = require('fs');
+var csvWriter = require('csv-write-stream')
+
+var writer = csvWriter({ headers: ["Location", "Product Number", "Quantity"]})
+writer.pipe(fs.createWriteStream('out.csv'))
+
 
 function orderPickLocation(orderSingle, orderDouble){
 orderSingle.sort(sortFunction);
 orderDouble.sort(sortFunction);
 order.push(orderSingle, orderDouble);
 
-order.forEach(function(element) {
-  console.log(element);
-}, this);
+for(let i =0; i < order.length; i++){
+  writer.write([`${order[0][i][0]}`, `${ order[0][i][1]}`, `${order[0][i][2]}`])
+}
 
+writer.end()
 }
 
 function sortFunction(a, b) {
